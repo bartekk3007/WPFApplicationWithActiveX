fcef9f89b0046cebcd930f990528eece7a9ebab2
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-04 23:23:08 +0200

    Add files via upload

---
+++ 7/HostingAxInWpf/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HostingAxInWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            // Create the ActiveX control.
+            AxWMPLib.AxWindowsMediaPlayer axWmp = new AxWMPLib.AxWindowsMediaPlayer();
+
+            // Assign the ActiveX control as the host control's child.
+            host.Child = axWmp;
+
+            // Add the interop host control to the Grid
+            // control's collection of child controls.
+            this.grid1.Children.Add(host);
+
+            // Play a .wav file with the ActiveX control.
+            axWmp.URL = @"C:\Users\48571\OneDrive\Pulpit\COMLab3\COMLab3\7\Ring01.wav";
+        }
+    }
+}
